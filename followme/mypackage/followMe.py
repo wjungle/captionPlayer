aa74af5a2f42941a3edadf31f9e632bc525a7c61
jungle wang <jungle.wang@wiess.biz>
2022-01-20 11:45:01 +0800

    add: toolbar and song object
    1. subtitles and song object is transfered to toolbar
    2. the status of song is in song object
    3. fix bug of playing status and button icon
    
    Signed-off-by: jungle wang <jungle.wang@wiess.biz>

--- followme/mypackage/followMe.py
+++ followme/mypackage/followMe.py
@@ -14,10 +14,16 @@ import math
 import pygame as pyg
 import threading
 
+songStatus = {
+    'INIT' : 0,
+    'PLAYING' : 1,
+    'PAUSE' : 2
+}
+
 def window():
-    global frameShow, subtitles, labelPage
+    global frameShow, subtitles, labelPage, toolbar
     win = tk.Tk()
-    win.geometry("860x380")
+    win.geometry("860x390")
     #win.iconbitmap("followme.ico")
     tmp = open("tmp.ico","wb+")
     tmp.write(base64.b64decode(img))
@@ -29,20 +35,6 @@ def window():
     # Initialze Pygame Mixer
     pyg.mixer.init()
 
-    # Create Menu
-    filemenu = tk.Menu(win)
-    win.config(menu = filemenu)
-        
-    # Add Add srt menu
-    add_srt_menu = tk.Menu(filemenu, tearoff=0)
-    add_srt_menu.add_command(label = '開啟檔案...', command = add_srt)
-    add_srt_menu.add_command(label = '開啟連接...', command = open_yt)
-    add_srt_menu.add_command(label = '離開程式', command = win.destroy)
-    filemenu.add_cascade(label = "檔案", menu = add_srt_menu)
-    filemenu.add_cascade(label = "說明")    
-
-    
-    
     # 字幕顯示區
     frameToolbar  = tk.Frame(win, 
                          #width = 860, 
@@ -52,40 +44,113 @@ def window():
     frameShow = tk.Frame(win, 
                          width = 860, 
                          height = 300)
-    
-    # Toolbar
-    btnFirst = tk.Button(frameToolbar, text="|<", width=3)
-    btnPrev = tk.Button(frameToolbar, text="<", width=3)
-    btnNext = tk.Button(frameToolbar, text=">", width=3)
-    btnBottom = tk.Button(frameToolbar, text=">|", width=3)
-    btnFirst.grid(row=0, column=0, padx=2, pady=2)
-    btnPrev.grid(row=0, column=1, padx=2, pady=2)
-    btnNext.grid(row=0, column=2, padx=2, pady=2)        
-    btnBottom.grid(row=0, column=3, padx=2, pady=2) 
-    
     # Create Status Bar
     frameStatusbar = tk.Frame(win, 
                          height = 20, 
                          relief="raised",
                          borderwidth = 2) 
-
+    
+    # tool bar
+    toolbar = Toolbar(frameToolbar)
+    # main frame
+    subtitles = Subtitle(6)
+    # status bar
     labelPage = tk.Label(frameStatusbar, text="")
+    
+    toolbar.setSubsCmd(subtitles)
+    #toolbar.setPageBtnEn()
 
     frameToolbar.pack(side = "top", fill = "x")  
     frameShow.pack()
     frameStatusbar.pack(fill="x", side="bottom", ipady=2)
     labelPage.pack(anchor="w")
-
-    subtitles = Subtitle(6)
-    btnFirst.config(command = subtitles.First)
-    btnPrev.config(command = subtitles.Prev)
-    btnNext.config(command = subtitles.Next)
-    btnBottom.config(command = subtitles.Bottom)
+    
+    # Create Menu
+    filemenu = tk.Menu(win)
+    win.config(menu = filemenu)
+        
+    # Add Add srt menu
+    add_srt_menu = tk.Menu(filemenu, tearoff = 0)
+    #add_srt_menu.add_command(label = '開啟檔案...', command = add_srt)
+    add_srt_menu.add_command(label = '開啟檔案...', command = lambda:add_srt(toolbar))
+    add_srt_menu.add_command(label = '開啟連接...', command = open_yt)
+    add_srt_menu.add_command(label = '離開程式', command = win.destroy)
+    filemenu.add_cascade(label = "檔案", menu = add_srt_menu)
+    filemenu.add_cascade(label = "說明")        
     
     win.mainloop()
+    
+class Toolbar():
+    def __init__(self, frameToolbar):
+        self.btnFirst = 0
+        self.btnPrev = 0
+        self.btnNext = 0
+        self.btnBottom = 0
+        self.btnGPlay = 0
+        # Toolbar
+        self.btnFirst = tk.Button(frameToolbar, text="|<", width=3, state=tk.DISABLED)
+        self.btnPrev = tk.Button(frameToolbar, text="<", width=3, state=tk.DISABLED)
+        self.btnNext = tk.Button(frameToolbar, text=">", width=3, state=tk.DISABLED)
+        self.btnBottom = tk.Button(frameToolbar, text=">|", width=3, state=tk.DISABLED)
+        self.btnGPlay = tk.Button(frameToolbar, text = "▶", 
+                             width=3, 
+                             font=("新細明體",12), 
+                             state=tk.DISABLED)
+        self.btnFirst.grid(row=0, column=0, padx=2, pady=2)
+        self.btnPrev.grid(row=0, column=1, padx=2, pady=2)
+        self.btnNext.grid(row=0, column=2, padx=2, pady=2)
+        self.btnBottom.grid(row=0, column=3, padx=2, pady=2) 
+        self.btnGPlay.grid(row=0, column=4, padx=2, pady=2)
+        
+    def setSubsCmd(self, subtitles):
+        self.btnFirst.config(command = subtitles.First)
+        self.btnPrev.config(command = subtitles.Prev)
+        self.btnNext.config(command = subtitles.Next)
+        self.btnBottom.config(command = subtitles.Bottom)
+        
+    def setSongCmd(self, song):
+        self.btnGPlay.config(command = lambda:self.play(song), state=tk.NORMAL)
+        
+    def setPageBtnEn(self):
+        self.btnFirst.config(state=tk.NORMAL)
+        self.btnPrev.config(state=tk.NORMAL)
+        self.btnNext.config(state=tk.NORMAL)
+        self.btnBottom.config(state=tk.NORMAL)
+        
+    def setGPlayBtn(self, subtitles):
+        #self.btnGPlay.config(command = subtitles.play, state=tk.NORMAL)
+        pass
+    
+    # Play and pause selected srt's mp3
+    def play(self, song):
+        song.cancelTimer()
+        if (self.btnGPlay['text'] == "▶"):
+            if pyg.mixer.music.get_busy() == True:
+                pyg.mixer.music.play(loops=0, start = 0)
+                print("AB is playing, rewind g play")
+            elif song.getSongStatus() == songStatus['INIT']:
+                pyg.mixer.music.unload()
+                pyg.mixer.music.load(song.getSong())
+                pyg.mixer.music.play(loops=0)
+                print("no song is playing, toolbar's play")    
+            elif song.getSongStatus() == songStatus['PAUSE']:
+                pyg.mixer.music.unpause()
+                print("g play just pause, you press UNPAUSE")
+            self.btnGPlay['text'] = ("| |")
+            song.setSongStatus(songStatus['PLAYING'])
+        else:
+            if song.getSongStatus() == songStatus['INIT']:
+                print("AB play is playing DONE, you press PAUSE")
+                self.btnGPlay['text'] = "▶"
+            #elif song.getSongStatus() == songStatus['PLAYING']:
+            else:
+                print("g play is now playing, you press PAUSE")
+                self.btnGPlay['text'] = "▶"
+                pyg.mixer.music.pause()
+                song.setSongStatus(songStatus['PAUSE'])
 
 class Subtitle():
-    global frameShow, labelPage
+    global frameShow, labelPage, song
     def __init__(self, pagesize):
         self.row = 0
         self.start = 0
@@ -119,9 +184,9 @@ class Subtitle():
                                       width = 15,
                                       font = ("Calibri",12))
                 self.tt_play_btn = tk.Button(frameShow,
-                                       text = "|＞", 
+                                       text = "▷", 
                                        width=3,
-                                       font=("新細明體",10),
+                                       font = ("Calibri",10),
                                        state=tk.DISABLED)
                 tt_sub_lbl = tk.Label(frameShow, 
                                   text = "subtitle",
@@ -140,9 +205,9 @@ class Subtitle():
                                            width = 5, 
                                            font = ("Calibri",12)))
                 self.play_btn.append(tk.Button(frameShow, 
-                                                text = "|＞", 
+                                                text = "▷", 
                                                 width = 3, 
-                                                font = ("新細明體",10),
+                                                font = ("Calibri",10),
                                                 state=tk.DISABLED))
                 self.subEng.append(tk.Label(frameShow, 
                                             text="", 
@@ -163,7 +228,6 @@ class Subtitle():
         self.datasize=len(subs) #資料筆數
         self.totpage=math.ceil(self.datasize/self.pagesize) #總頁數
         self.totfield=self.pagesize*self.totpage #總欄位數  
-        self.__set_song(song)
         self.tt_play_btn.configure(state=tk.NORMAL, command=self.play)
         
     def refresh_page(self):
@@ -208,7 +272,7 @@ class Subtitle():
      
     def Prev(self):  #上一頁
         if self.page > 0:
-            self.page -=1
+            self.page -= 1
             self.refresh_page()
 
     def Next(self): #下一頁
@@ -225,63 +289,77 @@ class Subtitle():
      
     # Play and pause selected srt's mp3
     def play(self):
-        self.__cancelTimer()
-        if(self.tt_play_btn['text'] == "|＞"):
-            if pyg.mixer.music.get_busy() == True:
-                pyg.mixer.music.play(loops=0, start = 0)
-                print("a")
-            elif self.playing == 0:
-                pyg.mixer.music.unload()
-                pyg.mixer.music.load(song)
-                pyg.mixer.music.play(loops=0)
-                print("c")
-            elif self.playing == 2:
-                pyg.mixer.music.unpause()
-                print(str(pyg.mixer.music.get_pos()) + " b")
-            self.tt_play_btn['text'] = ("| |")
-            self.playing = 1
-        else:
-            self.tt_play_btn['text'] = ("|＞")
-            pyg.mixer.music.pause()
-            print("d")
-            self.playing = 2
+        pass
     
+    # set AB play button
     def setAB(self, btn, start, duration):
         btn.configure(command = lambda:self.setABTimer(start, duration), state=tk.NORMAL)
 
     def playAB(self, start, duration):
         pyg.mixer.music.unload()
-        pyg.mixer.music.load(self.song)
+        # pyg.mixer.music.load(self.song)
+        pyg.mixer.music.load(song.getSong())
         pyg.mixer.music.play(loops=0, start = start)
         # print("helloworld %f - %f" % (start, duration))
-        self.playing = 0
+        song.setSongStatus(songStatus['PLAYING'])
 
     def setABTimer(self, start, duration):
         self.playAB(start, duration)
-        self.__cancelTimer()
+        song.cancelTimer()
+        song.newSongTimer(duration)
+     
+
+class Song():
+    def __init__(self, song):
+        self.song = song
+        self.playing = 0
+        self.stopTimer = 0   
+        
+    def getSong(self):
+        return self.song
+    
+    def setSongStatus(self, status):
+        self.playing = status
+        
+    def getSongStatus(self):
+        return self.playing
+        
+    def newSongTimer(self, duration):
         self.stopTimer = threading.Timer(duration, self.stop)
         self.stopTimer.start()
-        
+
+    # Play and pause selected srt's mp3
+    def play(self):
+        pyg.mixer.music.unload()
+        pyg.mixer.music.load(self.song)
+        pyg.mixer.music.play(loops=0)
+        print("song's play")
+            
     def stop(self):
         pyg.mixer.music.stop()
-        #print("stop")
+        song.setSongStatus(songStatus['INIT'])
+        print("song's stop")    
         
-    def __cancelTimer(self):
+    def cancelTimer(self):
         if self.stopTimer:
-            self.stopTimer.cancel()
+            self.stopTimer.cancel()        
         
 # Add Srt file Function
-def add_srt():
-    global subtitles, song, totpagevar
+def add_srt(toolbar):
+    global subtitles, song
     file = tkinter.filedialog.askopenfilename(initialdir = ".", 
                                               title = "選擇檔案", 
                                               filetypes =(("Subtitle Files","*.srt"),))
     if file:
-        song = file.replace(".srt",".mp3") # 為了找同檔名的mp3檔
+        songfile = file.replace(".srt",".mp3") # 為了找同檔名的mp3檔
         subs = srt.open(file, encoding = "utf-8")
     
-        subtitles.load_srt(subs, song)
+        subtitles.load_srt(subs, songfile)
+        song = Song(songfile)
         subtitles.refresh_page()
+        toolbar.setPageBtnEn()
+        toolbar.setGPlayBtn(subtitles)
+        toolbar.setSongCmd(song)
     
 def open_yt():
     pass
